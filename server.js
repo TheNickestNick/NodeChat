dce109aee88d0d221827d980daffa025e9016eb0
Nick Aceves <nickaceves@gmail.com>
2013-01-13 08:12:17 -0600

    Logging user join events. More layout tweaks to client. Clients now show when a user joins.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var server = require('http').createServer(handler)
   , io     = require('socket.io').listen(server)
   , fs     = require('fs');
@@ -36,6 +38,7 @@ function publishChatMessage(msg) {
 
 io.sockets.on('connection', function(socket) {
   socket.on('user-enter', function(data) {
+    console.log('user "%s" has joined the conversation', data.username);
     socket.broadcast.emit('user-enter', {username: data.username});
   });
   
