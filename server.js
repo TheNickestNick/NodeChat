28bb3a04eabb39cdea07e54ef5f35137b6e318ac
Nick Aceves <nickaceves@gmail.com>
2013-01-13 08:26:10 -0600

    Added the ability to post system messages to the chat from the console.

--- server.js
+++ server.js
@@ -42,7 +42,19 @@ io.sockets.on('connection', function(socket) {
     socket.broadcast.emit('user-enter', {username: data.username});
   });
   
+  socket.on('disconnect', function(data) {
+  });
+  
   socket.on('chat-message', function (data) {
     publishChatMessage({ username: data.username, text: data.text });
   });
+});
+
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(data) {
+  if (data.indexOf('system') === 0) {
+    io.sockets.emit('system-message', { text: data.substring(6, data.length) });
+  }
 });
